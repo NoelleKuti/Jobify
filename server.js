d60a25facc882be956dafd4bbd0eb0c56b54cb67
Noelle K <noeykuti@gmail.com>
2022-05-01 19:22:03 -0400

    added notFoundMiddleware and imported into server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js';
 
 const app = express();
 
+// middleware
 app.get('/', (req, res) => {
     res.send('Welcome!');
 })
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
