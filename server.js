c3b5e5fe396b27ec9b2b00436495079a7f879c51
Noelle K <noeykuti@gmail.com>
2022-05-01 18:16:44 -0400

    set up simple express server, added package.json for server outside of client directory

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('server running...');
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
