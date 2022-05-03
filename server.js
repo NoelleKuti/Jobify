d56986a98306f9bdf0cd704b6fdd18f0cc49634b
Noelle K <noeykuti@gmail.com>
2022-05-03 11:45:00 -0400

    moved app.listen() to async/await connectDB function so that app only renders if connection to database is successful

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ import dotenv from 'dotenv'
 dotenv.config();
 const app = express();
 
+import connectDB from './db/connect.js';
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
@@ -15,4 +18,12 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//connect to mongoose
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => console.log(`Server running on port ${port}`));        
+    } catch (error) {
+        console.log(error)
+    }
+}
