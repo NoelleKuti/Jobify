2f05ee00c1eb750981d60af6c9c1e4a6925c6af7
Noelle K <noeykuti@gmail.com>
2022-06-11 16:47:10 -0400

    created CustomAPIError class that exends the JavaScript built in Error class.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,21 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
+class CustomAPIError extends Error {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
 const register = async (req, res) => {
-    const user = await User.create(req.body);
+    const { name, email, password } = req.body;
+    
+    if (!name || !email || !password) {
+        throw new CustomAPIError('please provide all values' );
+    }
+
+    const user = await User.create({ name, email, password });
     res.status(StatusCodes.CREATED).json({ user });
 }
 const login = async (req,res) => {
