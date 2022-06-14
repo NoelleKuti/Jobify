804d4fc3502d80b1caafd1c243c8f3bfd7318be0
Noelle K <noeykuti@gmail.com>
2022-06-14 11:27:05 -0400

    used concurrently package to run frontend and backend from the root directory package.json

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const register = async (req, res) => {
 
     const user = await User.create({ name, email, password });
     const token = user.createJWT();
-    res.status(StatusCodes.CREATED).json({ user, token });
+    res.status(StatusCodes.CREATED).json({ user:{email: user.email, lastName: user.lastName, location: user.location, name: user.name}, token });
 }
 const login = async (req,res) => {
     res.send('login user');
--- models/User.js
+++ models/User.js
@@ -24,7 +24,8 @@ const UserSchema = new mongoose.Schema({
     password: { 
         type: String,
         required: [true, 'Please provide password'], 
-        minLength:6, 
+        minLength: 6, 
+        select: false,
     },
     lastName: { 
         type: String,
